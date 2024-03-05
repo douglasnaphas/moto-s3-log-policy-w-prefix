ec597451517b1b215465a4eac76be4da81506b32
Douglas Naphas <douglasnaphas@gmail.com>
2024-03-05 17:21:06 -0500

    Show that a prefix in the bucket policy fails

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import boto3
 from moto import mock_aws
-
+import pytest
 
 @mock_aws
-def test():
+def test_no_prefix_in_policy():
     s3_client = boto3.client("s3", region_name="us-east-1")
     assert len(s3_client.list_buckets()["Buckets"]) == 0
     s3_client.create_bucket(Bucket="my-bucket1234")
@@ -39,3 +39,39 @@ def test():
     bucket_logging2 = s3_client.get_bucket_logging(Bucket="my-bucket1234")
     print(bucket_logging2)
 
+@mock_aws
+def test_prefix_in_policy():
+    s3_client = boto3.client("s3", region_name="us-east-1")
+    assert len(s3_client.list_buckets()["Buckets"]) == 0
+    s3_client.create_bucket(Bucket="my-bucket1234")
+    assert len(s3_client.list_buckets()["Buckets"]) == 1
+    bucket_logging1 = s3_client.get_bucket_logging(Bucket="my-bucket1234")
+    print(bucket_logging1)
+    s3_client.create_bucket(Bucket="logging-bucket1234")
+    logging_bucket1234_policy = """{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "S3ServerAccessLogsPolicy",
+                "Effect": "Allow",
+                "Principal": {"Service": "logging.s3.amazonaws.com"},
+                "Action": ["s3:PutObject"],
+                "Resource": "arn:aws:s3:::logging-bucket1234/my-bucket1234/*"
+            }
+        ]
+    }"""
+    put_bucket_policy_response = s3_client.put_bucket_policy(
+        Bucket="logging-bucket1234", Policy=logging_bucket1234_policy
+    )
+    print(f"{put_bucket_policy_response=}")
+    s3_client.put_bucket_logging(
+        Bucket="my-bucket1234",
+        BucketLoggingStatus={
+            "LoggingEnabled": {
+                "TargetBucket": "logging-bucket1234",
+                "TargetPrefix": "my-bucket1234/",
+            }
+        },
+    )
+    bucket_logging2 = s3_client.get_bucket_logging(Bucket="my-bucket1234")
+    print(bucket_logging2)
